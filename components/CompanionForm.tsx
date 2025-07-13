d5b6527612abfd6f4efd07a776b0d4c197c91d86
mohd-hassan17 <mohdhassana04@gmail.com>
2025-07-14 00:42:04 +0530

    Implemented auth billing database

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -24,6 +24,8 @@ import {
 } from "@/components/ui/select"
 import { subjects } from "@/constants";
 import { Textarea } from "./ui/textarea";
+import { createCompanion } from "@/lib/actions/companion.actions";
+import { redirect } from "next/navigation";
 
 const formSchema = z.object({
     name: z.string().min(1, { message: 'Companion is required.' }),
@@ -50,9 +52,17 @@ const CompanionForm = () => {
     })
 
     // 2. Define a submit handler.
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
 
-        console.log(values)
+        const companion = await createCompanion(values);
+
+        console.log(values);
+        if(companion){
+            redirect('/companions/${companion.id}')
+        }else{
+            console.log('Failed to create companion');
+            redirect('/');
+        }
     }
 
     return (
